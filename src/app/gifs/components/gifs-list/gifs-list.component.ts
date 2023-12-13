34cd12fd233900c510f96751204c15e9495f8663
AdriDev <adriDev1993@gmail.com>
2023-12-13 16:33:08 +0100

    gifs components created

---
+++ src/app/gifs/components/gifs-list/gifs-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gifs-list',
+  templateUrl: './gifs-list.component.html',
+  styleUrls: ['./gifs-list.component.css']
+})
+export class GifsListComponent {
+
+}
--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomeComponent } from './pages/home/home.component';
+import { SearchBoxComponent } from './components/search-box/search-box.component';
+import { GifsListComponent } from './components/gifs-list/gifs-list.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeComponent,
+    SearchBoxComponent,
+    GifsListComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    HomeComponent
   ]
 })
 export class GifsModule { }
